db3d89d138a68c33563473898f7895b7b6f2be0a
Bunty3017 <bunty.rahul2517@gmail.com>
2026-01-30 12:07:27 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as modern JS
+
+// alert("Hello World!"); we are usibg nodejs, not browsers
+
+let name = "John"; // string literal
+let age = 25; // number literal
+let isApproved = false; // boolean literal
+let firstName = undefined; // undefined literal
+let selectedColor = null; // null literal
+
+
+// number => 2 to power 53
+// bigint => larger than 2 to power 53
+// boolean => true or false
+// string => "" '' ``
+// null => empty value
+// undefined => value not assigned
+// symbol => unique identifiers
+// object => key-value pairs
+// function => reusable blocks of code
+
+console.log(typeof name);
